e736a7da844fe12ed2965f4ee9aca232a9e094af
daniela-gomez <danielaandrea.gomez.alu@iescampanillas.com>
2021-02-27 23:30:03 +0100

    Primer commit

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author Daniela Gómez
+ * @version 1.0
+ * @category ejercicio de clase
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola mundo mi primer ejecutable desde Eclipse");
+
+	}
+
+}
